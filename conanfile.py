842c7474a68a24faa0c58df84d4387310ffdce10
Arjun Kalsi <ARJUNKALSI13@GMAIL.COM>
2024-09-29 09:59:28 -0400

    Basic code to understand Boost ASIO functionality

--- conanfile.py
+++ conanfile.py
@@ -1,10 +1,20 @@
 from conan import ConanFile
+from conan.tools.cmake import CMakeToolchain, CMakeDeps, CMake
 
-class ConanPackage(ConanFile):
-    name = "network-monitor"
-    version = "0.1.0"
-    settings = "os", "arch", "compiler", "build_type"
-    generators = "CMakeDeps"
+class MyBoostProjectConan(ConanFile):
+    settings = "os", "compiler", "build_type", "arch"
+    requires = "boost/1.78.0"
+    generators = "CMakeToolchain", "CMakeDeps"
 
-    def requirements(self):
-        pass
+    def configure(self):
+        self.options["boost"].shared = False
+
+    def imports(self):
+        self.copy("*.dll", dst="bin", src="bin")
+        self.copy("*.dylib*", dst="bin", src="lib")
+        self.copy("*.so*", dst="bin", src="lib")
+
+    def build(self):
+        cmake = CMake(self)
+        cmake.configure()
+        cmake.build()
--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,69 @@
+#include <boost/asio.hpp>
+#include <boost/system/error_code.hpp>
+
+#include <iomanip>
 #include <iostream>
+#include <thread>
+
+using tcp = boost::asio::ip::tcp;
+
+void Log(boost::system::error_code ec)
+{
+    std::cerr << "[" << std::setw(14) << std::this_thread::get_id() << "] "
+        << (ec ? "Error: " : "OK")
+        << (ec ? ec.message() : "")
+        << std::endl;
+}
+
+void OnConnect(boost::system::error_code ec)
+{
+    Log(ec);
+}
 
 int main()
 {
-    std::cout << "It works" << std::endl;
+    std::cerr << "[" << std::setw(14) << std::this_thread::get_id() << "] main"
+        << std::endl;
+
+    // Always start with an I/O context object.
+    boost::asio::io_context ioc{};
+
+    // Create an I/O object. Every Boost.Asio I/O object API needs an io_context
+    // as the first parameter.
+    tcp::socket socket{ boost::asio::make_strand(ioc) };
+
+    size_t nThreads{ 4 };
+
+    // Under the hood, socket.connect uses I/O context to talk to the socket
+    // and get a response back. The response is saved in ec.
+    boost::system::error_code ec{};
+    tcp::resolver resolver{ ioc };
+    auto resolverIt{ resolver.resolve("google.com", "80", ec) };
+    if (ec) {
+        Log(ec);
+        return -1;
+    }
+    for (size_t idx{ 0 }; idx < nThreads; ++idx) {
+        socket.async_connect(*resolverIt, OnConnect);
+    }
+
+    // We must call io_context::run for asynchronous callbacks to run.
+    // Below [&ioc]() {} is a lambda function capturing ioc by reference from environment and passing to thread.
+    // Note: Why passing lambda function to thread? So thread here needed a function to be passed. since what we
+    // want to achieve from this thread is very limited (just ioc.run() ) its more convenient to use lambda here.
+    // And you cant pass ioc.run() directly since this function is part of a different class and first we need
+    // to know the object before running a function of the class that that object belongs to.
+    std::vector<std::thread> threads{};
+    threads.reserve(nThreads); // reserving fixed memory in advance so that we dont need to use dynamic memory allocation.
+    for (size_t idx{ 0 }; idx < nThreads; ++idx) {
+        threads.emplace_back([&ioc]() {
+            ioc.run();
+            });
+    }
+
+    for (size_t idx{ 0 }; idx < nThreads; ++idx) {
+        threads[idx].join();
+    }
+
     return 0;
 }
